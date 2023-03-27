371c6429a4195cebb4ff1434f21050996f11c6f3
wismoon <49384700+wismoon@users.noreply.github.com>
2023-03-27 11:46:39 +0700

    switch_flutter

---
+++ switch_flutter/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:switch_flutter/Pages/Home_Page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
