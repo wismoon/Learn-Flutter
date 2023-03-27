663ad4295132148fc8cb8358936e7225ee393432
wismoon <49384700+wismoon@users.noreply.github.com>
2023-03-27 11:46:21 +0700

    switch_flutter

---
+++ switch_flutter/lib/Pages/Home_Page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  bool StatusSwitch = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Switch"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+              SwitchListTile(
+                
+                activeColor: Colors.green[400],
+                inactiveThumbColor: Colors.red[400],
+            
+                value: StatusSwitch, 
+                onChanged: ((value) {
+                  setState(() {
+                    StatusSwitch = !StatusSwitch;
+                  });
+                })),
+            Text(
+              (StatusSwitch == false) ? "Tekan saya" : "Sudah Ditekan", 
+              style: TextStyle(fontSize: 34),),
+          ],
+        ),
+      ),
+    );
+  }
+}
